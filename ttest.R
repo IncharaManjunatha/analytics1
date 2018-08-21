af8aa01fe39ce365c6416b1139d38b02f098a596
IncharaManjunatha <incharakm4197@gmail.com>
2018-08-21 23:35:26 +0530

    21/08/2018

---
+++ ttest.R
@@ -0,0 +1,10 @@
+# t test two sample test
+
+(class1 = sample(60:100,25))
+(class2 = sample(60:100,30))
+
+(ttest2s1 = t.test(x=class1, y=class2))
+
+# https://github.com/dupadhyaya/analytics/blob/master/18a-HypoTests/26b-ttestindep.R
+
+# all tests are avalable in github
